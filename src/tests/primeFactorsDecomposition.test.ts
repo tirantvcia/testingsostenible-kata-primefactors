ada8f30e01d13c1fec110b5fb501ddfe02ff0257
tirantvcia <tirantvcia@gmail.com>
2023-04-03 12:39:03 +0200

    refactorizar codigo test en nuevas aserciones

--- src/tests/primeFactorsDecomposition.test.ts
+++ src/tests/primeFactorsDecomposition.test.ts
@@ -15,18 +15,23 @@ describe("Descomposición factores primos", () => {
     it("Un número primo devuelve el mismo número", () => {
         var result = primeFactors(2);
         expect(result).toMatchObject([2]);
+        expect(primeFactors(3)).toMatchObject([3])
     });
     it("Un número no primo devuelve su descomposicion en primos", () => {
         var result = primeFactors(2 * 2);
         expect(result).toMatchObject([2, 2]);
         expect(primeFactors(2 * 2 * 2)).toMatchObject([2, 2, 2])
-        expect(primeFactors(3)).toMatchObject([3])
         expect(primeFactors(3 * 3)).toMatchObject([3, 3])
-        expect(primeFactors(2 * 3)).toMatchObject([2, 3])
         expect(primeFactors(5 * 5)).toMatchObject([5, 5])
+
+    });
+    it("Un número no primo devuelve su descomposicion en primos ordenados menor a mayor", () => {
+        var result = primeFactors(2 * 2);
+        expect(primeFactors(2 * 3)).toMatchObject([2, 3])
         expect(primeFactors(5 * 7 * 11 * 3)).toMatchObject([3, 5, 7, 11])
 
     });
+
 });
 
 
